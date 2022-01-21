57e221d980cc92e980bdd1fc20d4107407813f79
salem <salem.dahmani345@gmail.com>
2022-01-21 19:24:36 +0100

    nav bar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PoulinaApp';
+  sideBarOpen = true;
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
